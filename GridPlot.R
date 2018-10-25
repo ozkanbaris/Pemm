54d9c4b85dc087ae8183b43dcd9ddc1e7923c9ab
Baro <javaceylan@gmail.com>
2018-10-25 10:07:03 +0200

    test C

--- GridPlot.R
+++ GridPlot.R
@@ -4,6 +4,14 @@ postpem <- read_csv('PEMMPST.csv')
 prepem <- read_csv('PEMMPR.csv')
 
 melted_post<-melt(postpem, id.vars = "Persoon")
+melted_pre<-melt(postpem, id.vars = "Persoon")
 
-ggplot(data = melted_post, aes(x=Persoon, y=variable, fill=value)) + 
+
+
+
+
+
+ggplot(data = melted_post, aes(y=Persoon, x=variable, fill=value)) + 
+  geom_tile()
+ggplot(data = melted_pre, aes(y=Persoon, x=variable, fill=value)) + 
   geom_tile()
